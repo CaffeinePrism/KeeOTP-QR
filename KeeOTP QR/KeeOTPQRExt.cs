fd54e788fca94ee7a33a01bfb1ad186e029de123
Richard Li <nontextual@gmail.com>
2014-08-29 12:13:05 -0700

    does bare minimum

---
+++ KeeOTP QR/KeeOTPQRExt.cs
@@ -0,0 +1,62 @@
+﻿using KeePass.Plugins;
+using KeePassLib;
+using System;
+using System.Windows.Forms;
+
+namespace KeeOTPQR
+{
+    public sealed class KeeOTPQRExt : Plugin
+    {
+        private IPluginHost host = null;
+        private ToolStripItem otpDialogToolStripItem;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            this.host = host;
+
+            this.otpDialogToolStripItem = host.MainWindow.EntryContextMenu.Items.Add("QR",
+                null,
+                otpDialogToolStripItem_Click);
+
+            return true;
+        }
+        void otpDialogToolStripItem_Click(object sender, EventArgs e)
+        {
+            PwEntry entry;
+            if (GetSelectedSingleEntry(out entry))
+            {
+                ShowQR form = new ShowQR(entry, host);
+                form.ShowDialog();
+            }
+        }
+        private bool GetSelectedSingleEntry(out PwEntry entry)
+        {
+            entry = null;
+
+            var entries = this.host.MainWindow.GetSelectedEntries();
+            if (entries == null || entries.Length == 0)
+            {
+                MessageBox.Show("Please select an entry");
+                return false;
+            }
+            else if (entries.Length > 1)
+            {
+                MessageBox.Show("Please select only one entry");
+                return false;
+            }
+            else
+            {
+                // grab the entry that we care about
+                entry = entries[0];
+                return true;
+            }
+        }
+        public override void Terminate()
+        {
+            // Remove all of our menu items
+            ToolStripItemCollection menu = host.MainWindow.EntryContextMenu.Items;
+            menu.Remove(otpDialogToolStripItem);
+        }
+
+    }
+}
---
+++ KeeOTP QR/ShowQR.cs
@@ -0,0 +1,108 @@
+﻿using KeePass.Plugins;
+using KeePassLib.Security;
+using System;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using ZXing;
+
+namespace KeeOTPQR
+{
+    public partial class ShowQR : Form
+    {
+        const string keyParameter = "key";
+        const string typeParameter = "type";
+
+        private readonly KeePassLib.PwEntry entry;
+        private readonly IPluginHost host;
+        private string title;
+        private string type = "totp";
+        private ProtectedString data;
+        private ProtectedString key;
+
+
+        public ShowQR(KeePassLib.PwEntry entry, IPluginHost host)
+        {
+            this.host = host;
+            this.entry = entry;
+            InitializeComponent();
+
+            this.title = entry.Strings.Get("Title").ReadString();
+            this.data = entry.Strings.Get("otp");
+            getData(data.ReadString());
+
+            IBarcodeWriter writer = new BarcodeWriter {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = pictureBox1.Width,
+                    Height = pictureBox1.Height
+                }
+            };
+            var result = writer.Write(buildURL().ReadString());
+            var barcodeBitmap = new Bitmap(result);
+            pictureBox1.Image = barcodeBitmap;
+        }
+        public ProtectedString buildURL()
+        {
+            char[] delimiter = { ':' };
+            string url = "otpauth://";
+            url += type;
+            url += "/";
+            url += title;
+            url += "?secret=";
+            url += key.ReadString();
+            url += "&issuer=";
+            url += title.Split(delimiter)[0];
+
+            return new ProtectedString(true, url);
+        }
+        public void getData(string data)
+        {
+            NameValueCollection parameters = ParseQueryString(data);
+
+            if (parameters[keyParameter] == null)
+                throw new ArgumentException("Must have a key in the data");
+
+            this.key = new ProtectedString(true, parameters[keyParameter]);
+            if (parameters[typeParameter] != null)
+                this.type = parameters[typeParameter];
+        }
+
+        /// <remarks>
+        /// Hacky query string parsing.  This was done due to reports
+        /// of people with just a 3.5 or 4.0 client profile getting errors
+        /// as the System.Web assembly where .net's implementation of
+        /// Url encoding and query string parsing is locate.
+        /// 
+        /// This should be fine since the only thing stored in the string
+        /// that needs to be encoded or decoded is the '=' sign.
+        /// </remarks>
+        private static NameValueCollection ParseQueryString(string data)
+        {
+            var collection = new NameValueCollection();
+
+            var parameters = data.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var parameter in parameters)
+            {
+                if (parameter.Contains("="))
+                {
+                    var pieces = parameter.Split('=');
+                    if (pieces.Length != 2)
+                        continue;
+
+                    collection.Add(pieces[0], pieces[1].Replace("%3d", "="));
+                }
+            }
+
+            return collection;
+        }
+        public Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+    }
+}
